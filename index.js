130d2bb3e7bf9a0f763c1dde43b04dc824acaeb5
woldejamawolde <wolde.jama@m0ringaschool.com>
2023-10-12 04:52:25 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(arrName,message){
+    let msg=[];
+
+for(let x=0;x<arrName.length;x++){
+const h=`Thank you, ${arrName[x]}, for the wonderful ${message} gift!`
+msg.push(h)}
+return msg
+}
+
+function countDown(num){
+let count=0;
+while(count<=num)
+console.log(num--)
+}
